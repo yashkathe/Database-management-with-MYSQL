0d5ce97df793c8e834677a8db73ba0f03237ddb6
Yash Kathe <yashkathe123@gmail.com>
2024-01-31 20:55:28 -0800

    completed splitting and joining

---
+++ Splitting and Joining Data/12-relationships.sql
@@ -0,0 +1,13 @@
+CREATE TABLE employees (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  first_name VARCHAR(300) NOT NULL,
+  last_name VARCHAR(300) NOT NULL,
+  birthdate DATE NOT NULL,
+  email VARCHAR(200) UNIQUE NOT NULL
+);
+
+CREATE TABLE intranet_accounts (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  email VARCHAR(200) REFERENCES employees (email) ON DELETE CASCADE,
+  password VARCHAR(200) NOT NULL
+);
---
+++ Splitting and Joining Data/14-more-relations.sql
@@ -0,0 +1,26 @@
+CREATE TABLE company_buildings (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(300) NOT NULL
+);
+
+CREATE TABLE teams (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(300) NOT NULL,
+  building_id INT REFERENCES company_buildings ON DELETE SET NULL
+);
+
+CREATE TABLE employees (
+  id INT PRIMARY KEY AUTO_INCREMENT, 
+  first_name VARCHAR(300) NOT NULL,
+  last_name VARCHAR(300) NOT NULL,
+  birthdate DATE NOT NULL,
+  -- email VARCHAR(200) REFERENCES intranet_accounts ON DELETE
+  email VARCHAR(200) UNIQUE NOT NULL,
+  team_id INT DEFAULT 1 REFERENCES teams ON DELETE SET DEFAULT
+);
+
+CREATE TABLE intranet_accounts (
+  id INT PRIMARY KEY AUTO_INCREMENT, -- MySQL
+  email VARCHAR(200) REFERENCES employees (email) ON DELETE CASCADE,
+  password VARCHAR(200) NOT NULL
+);
